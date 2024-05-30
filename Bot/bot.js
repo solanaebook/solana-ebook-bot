66b8726098f2ad4a562c427f19976cb9b71a1367
solanaebook <solanaebook@gmail.com>
2024-05-30 18:44:05 +0300

    Delete bot.js

--- Bot/bot.js
+++
@@ -1,7 +0,0 @@
-const { Telegraf } = require('telegraf')
-const TOKEN = "6852098925:AAEbvMLf31rczz3WQwmDeO9P4MImoyjlBdQ"
-const bot = new Telegraf(TOKEN)
-
-bot.start((ctx) => ctx.reply('Welcome'))
-
-bot.launch()
